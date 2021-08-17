b3c389d78e29cb588cab61f45409c13e481b67f7
moradalkhatib <muradalkhateb@icloud.com>
2021-08-17 22:51:47 +0300

    post

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 "use strict";
-
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const jwksClient = require("jwks-rsa");
-const mongoose=require('mongoose')
 const app = express();
 app.use(cors());
+app.use(express.json());
 const PORT = process.env.PORT;
-const bookController=require('./controllers/book.controller')
+
 const client = jwksClient({
   jwksUri: `https://${process.env.REACT_APP_DOMAIN}/well/jwks.json`,
 });
@@ -20,12 +19,88 @@ const getKey = (header, callback) => {
   });
 };
 
+
+
+
+
+const mongoose=require('mongoose')
+
+
 //database
 
-mongoose.connect('mongodb://localhost:27017/book',{useNewUrlParser: true})
+mongoose.connect('mongodb://localhost:27017/newBooks',{useNewUrlParser: true ,useUnifiedTopology: true})
+const bookSchema= new mongoose.Schema({
+  title: String,
+  description: String,
+  status: String,
+ })
+ const ownerSchema= new mongoose.Schema({
+     email: String,
+     book:[bookSchema]
+    })
+ 
+ 
+ const myBook=mongoose.model('book',bookSchema)
+ const myOwnerBook=mongoose.model('owner',ownerSchema)
+ 
+ const seedBook=()=>{
+     const murat=new myOwnerBook({
+         email: 'moradnnn7@gmail.com',
+         book:[ {
+             title: 'Java',
+             description: 'talk about Java programming language',
+             status: 'available',
+            
+         },
+         {
+             title: 'JavaScript',
+             description: 'talk about JavaScript programming language',
+             status: 'available',
+            
+         }]
+        })
+ 
+    
+     murat.save()
+       
+     console.log(murat)
+ 
+ }
+ 
+// seedBook();
+app.get("/books",(req,res)=>{
+   let {email}=req.query
+   console.log(email);
+  myOwnerBook.find({email:email},(error,data)=>{
+    if(error){res.send("We Have something happen")}
+    else{
+      res.send(data[0].book)
+    }
 
-app.get("/books",bookController)
+  })
 
+})
+
+app.post("/books-add",(req,res)=>{
+  console.log(req.body);
+  let {email,bookTitle,bookDescription,bookStatus}=req.body
+
+  myOwnerBook.find({email:email},(error,data)=>{
+    console.log("a");
+    if(error){res.send("We Have something happen")}
+    else{
+      data[0].book.push({
+        title:bookTitle,
+        description:bookDescription,
+        status:bookStatus
+      })
+      data[0].save()
+      res.send(data[0].book)
+    }
+
+  })
+
+})
 
 
 app.get("/test", (request, response) => {
